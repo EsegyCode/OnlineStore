48ccb612e6bd6492ad22498ff214a2806fac69ad
EsegyCode <djkometad@gmail.com>
2024-10-01 22:33:09 +0300

    SecondCommitDBADD

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
-
+from sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqllite:///OnlineShop.bd'
+db=SQLAlchemy
+
+
+class Item(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100), nullable=False)
+    price=db.Column(db.Integer, nullable=False)
+    isActive=db.Column(db.Boolean, default=True)
+
+
 
 @app.route('/')
 def index():
@@ -13,9 +24,7 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
 
 
 if __name__ == "__main__":
