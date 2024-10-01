311b92a90b311ced58195ab9801f519f3cf675c1
EsegyCode <djkometad@gmail.com>
2024-10-02 01:43:47 +0300

    BackupWithoutLogin

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///OnlineShop.bd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'  # Для захисту сесій
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'  # Вказуємо, що неавторизовані користувачі будуть перенаправлені на 'login'
-
-# Модель користувача
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(200), nullable=False)
 
 # Модель товару
 class Item(db.Model):
@@ -26,13 +14,8 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-# Головна сторінка магазину (лише для авторизованих користувачів)
+# Головна сторінка магазину
 @app.route('/')
-@login_required
 def index():
     items = Item.query.order_by(Item.price).all()
     return render_template('index.html', data=items)
@@ -41,9 +24,8 @@ def index():
 def about():
     return render_template('about.html')
 
-# Створення товару (лише для авторизованих користувачів)
+# Створення товару
 @app.route('/create', methods=['POST', 'GET'])
-@login_required
 def create():
     if request.method == "POST":
         title = request.form['title']
@@ -57,9 +39,8 @@ def create():
             return "Помилка в роботі БД"
     return render_template('create.html')
 
-# Редагування товару (лише для авторизованих користувачів)
+# Редагування товару
 @app.route('/edit/<int:id>', methods=['POST', 'GET'])
-@login_required
 def edit(id):
     item = Item.query.get_or_404(id)
     if request.method == "POST":
@@ -72,9 +53,8 @@ def edit(id):
             return "Помилка під час редагування товару"
     return render_template('edit.html', item=item)
 
-# Видалення товару (лише для авторизованих користувачів)
+# Видалення товару
 @app.route('/delete/<int:id>')
-@login_required
 def delete(id):
     item = Item.query.get_or_404(id)
     try:
@@ -83,45 +63,6 @@ def delete(id):
         return redirect('/')
     except:
         return "Помилка при видаленні товару"
-
-# Реєстрація
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        user = User(username=username, password=hashed_password)
-        try:
-            db.session.add(user)
-            db.session.commit()
-            flash('Реєстрація пройшла успішно. Можете увійти.')
-            return redirect(url_for('login'))
-        except:
-            flash('Користувач з таким іменем вже існує.')
-    return render_template('register.html')
-
-# Логін
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            next_page = request.args.get('next')  # Отримуємо наступну сторінку, якщо користувач був перенаправлений
-            return redirect(next_page) if next_page else redirect(url_for('index'))  # Переходимо на next або головну
-        else:
-            flash('Неправильний логін або пароль.')
-    return render_template('login.html')
-
-# Вихід
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
